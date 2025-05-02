b79a48f8bf210e822e7ba7f48f9d957ff4446a35
suryaraj.technical.idm@gmail.com <123456789+suryaraj-thirumuruga-idm@users.noreply.github.com>
2025-05-02 18:42:37 +0530

    js

---
+++ js/script.js
@@ -0,0 +1,106 @@
+// alert('external js');
+// console.log("Hello welocme");
+
+
+// var Number = 12345;
+// var Number =345;
+
+// let Number = 1234;
+// Number =9876;
+
+// console.log(Number);
+
+
+// const y =90;
+// // y=99
+// console.log(y);
+
+// alert('warning..')
+// prompt('enter Your Name')
+// confirm('r u exit..')
+// print()
+// close()
+// open('')
+
+// let num = 10 ;
+
+// console.log(num);
+// console.log(num.toString());
+
+// let a = "20";
+
+// console.log(a);
+// console.log(parseInt(a));
+
+// let Name = "  SURYARAJ ";
+
+// console.log(Name);
+// console.log(Name.concat(Name));
+// console.log(Name.indexOf('R'));
+// console.log(Name.lastIndexOf("R"));
+// console.log(Name.length);
+// console.log(Name.repeat(5));
+// console.log(Name.toLowerCase());
+// console.log(Name.trim());
+
+// Name[11] ="Erode";
+
+// console.log(Name);
+
+
+// console.log(Math.E);
+// console.log(Math.PI);
+// console.log(Math.ceil(9.1));
+// console.log(Math.floor(90.9));
+
+// console.log(Math.max(99,80));
+// console.log(Math.min(88,77));
+// console.log(Math.pow(2,5));
+// console.log(Math.random());
+// console.log(Math.sqrt(64));
+
+// +,-,/,*,%
+// = !=
+// < > <= => == !=
+// AND &&  OR ||   NOT !
+// ++ --
+// value ? "true" : "false";
+
+if(10<4){
+       console.log("runing...");
+}
+else if((40<60)&&(60<7)){
+       console.log("and done...");
+       
+}
+
+else if((40<50)||(60<50)){
+       console.log("fiunal runing..");
+       
+}
+
+else{
+       console.log("wrong");
+       
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
